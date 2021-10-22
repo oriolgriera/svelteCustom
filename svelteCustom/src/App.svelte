d741f059b47f6a9a95b06a591fe16709f453b3c6
oriolgriera <oriolgrierajimenez@gmail.com>
2021-10-22 10:13:55 +0200

    Implementation of svelte custom app using proi-ui

---
+++ svelteCustom/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Button, Input} from 'proi-ui'
+	export let name  = "";
+	export let familyname  = "";
+	export let submit = false;
+	
+	function submitInfo() {
+		submit = true;
+	}
+
+	function resetInfo() {
+		submit = false;
+		name  = "";
+		familyname  = "";
+	}
+	</script>
+
+<main>
+	<Input label="Name" bind:value={name}/>
+	<Input label="Family Name" bind:value={familyname}/>
+	<Button on:click={submitInfo}>Submit</Button>
+	<Button type="secondary" on:click={resetInfo}>Reset</Button>
+	{#if submit}
+	<p>Your name is {name} and your family name is {familyname}.</p>
+	<!-- <Alert type="success">Correctly Submitted</Alert> -->
+    {:else}
+	<p>Please submit the info.</p>
+    {/if}
+</main>
